86184bf9d0fa5a72c526db50821f0b3b4edd576a
Padma Shree <150917358+PadmashreeArunprasath@users.noreply.github.com>
2025-09-17 14:46:13 +0530

    commit

--- Spring_Restfulapi/src/main/java/com/tnsif/springRestapi/RestControllerExample.java
+++ Spring_Restfulapi/src/main/java/com/tnsif/springRestapi/RestControllerExample.java
@@ -1,6 +1,9 @@
 package com.tnsif.springRestapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,16 @@ public class RestControllerExample {
 	public String show() {
 		return "Welcome";
 	}
+	
+	@GetMapping("/heloo")
+	public String greetings(@RequestParam String name) {
+		return "HI "+name;
+	}
+	
+	@GetMapping("/heloo/{name}")
+	public String message(@PathVariable String name) {
+		return "Welcome "+name;
+	}
+	
+	
 }
